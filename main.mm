2e35ec50ad719f2ffb215be82c2f1da6f6c4ad28
ichitaso <ichitaso@MBP.local>
2014-06-02 18:44:57 +0900

    Layout change

--- main.mm
+++ main.mm
@@ -17,21 +17,21 @@ int main(int argc, char **argv, char **envp) {
         
         NSTask *task = [[NSTask alloc] init];
         [task setLaunchPath: @"/bin/launchctl"];
+        NSArray *unload = [NSArray arrayWithObjects: @"unload", @"/System/Library/LaunchDaemons/com.apple.syslogd.plist", nil];
+        NSArray *load = [NSArray arrayWithObjects: @"load", @"/System/Library/LaunchDaemons/com.apple.syslogd.plist", nil];
+        
+        NSString *str1 = @"#*.* /var/log/syslog\n";
+        NSString *str2 = @"*.* /var/log/syslog\n";
         
         NSFileManager *manager = [NSFileManager defaultManager];
         
         if ([manager removeItemAtPath:clearCache error:nil]) {
             [manager removeItemAtPath:logPath error:nil];
             
-            NSString *str1 = @"#*.* /var/log/syslog\n";
             [str1 writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
-            
-            NSArray *unload = [NSArray arrayWithObjects: @"unload", @"/System/Library/LaunchDaemons/com.apple.syslogd.plist", nil];
             
             [task setArguments:unload];
             [task launch];
-            
-            NSArray *load = [NSArray arrayWithObjects: @"load", @"/System/Library/LaunchDaemons/com.apple.syslogd.plist", nil];
             
             [task setArguments:load];
             [task launch];
@@ -39,23 +39,15 @@ int main(int argc, char **argv, char **envp) {
             [manager createFileAtPath:filePath contents:nil attributes:nil];
             
             if (syslogEnabled) {
-                NSString *str1 = @"#*.* /var/log/syslog\n";
                 [str1 writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
-                
-                NSArray *unload = [NSArray arrayWithObjects: @"unload", @"/System/Library/LaunchDaemons/com.apple.syslogd.plist", nil];
                 
                 [task setArguments:unload];
                 [task launch];
                 
-                NSArray *load = [NSArray arrayWithObjects: @"load", @"/System/Library/LaunchDaemons/com.apple.syslogd.plist", nil];
-                
                 [task setArguments:load];
                 [task launch];
             } else {
-                NSString *str2 = @"*.* /var/log/syslog\n";
                 [str2 writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
-                
-                NSArray *load = [NSArray arrayWithObjects: @"load", @"/System/Library/LaunchDaemons/com.apple.syslogd.plist", nil];
                 
                 [task setArguments:load];
                 [task launch];
